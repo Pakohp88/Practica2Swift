b66c6fcd697e70f1d3ac8d0d20b81b4b6cea8269
Francisco Hernandez <pakohp@hotmail.com>
2022-11-05 11:01:34 -0600

    Estrucuta pratica 2 swift

---
+++ AudioPlayer/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  AudioPlayer
+//
+//  Created by Francisco Hernandez on 05/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var btnPlay: UIButton!
+    @IBOutlet var btnStop: UIButton!
+    @IBOutlet var sliderDuration: UISlider!
+    @IBOutlet var sliderVolume: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func playButtonPress(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonPress(_ sender: Any) {
+    }
+    
+    
+    @IBAction func durationSlider(_ sender: Any) {
+    }
+    
+    
+    @IBAction func volumeSlider(_ sender: Any) {
+    }
+    
+}
+
